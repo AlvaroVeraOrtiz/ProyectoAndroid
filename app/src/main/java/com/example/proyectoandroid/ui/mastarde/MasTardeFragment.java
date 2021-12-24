e70742ff8eaf676eba27c3f6b59f738d62f64837
GuillermoAguado <guilleam@uma.es>
2021-12-24 18:10:56 +0100

    🤙Add Ver Mas Tarde to menu

--- app/src/main/java/com/example/proyectoandroid/ui/mastarde/MasTardeFragment.java
+++ app/src/main/java/com/example/proyectoandroid/ui/mastarde/MasTardeFragment.java
@@ -1,4 +1,4 @@
-package com.example.proyectoandroid.ui.gallery;
+package com.example.proyectoandroid.ui.mastarde;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.proyectoandroid.R;
-import com.example.proyectoandroid.databinding.FragmentGalleryBinding;
+import com.example.proyectoandroid.databinding.FragmentMasTardeBinding;
 
-public class GalleryFragment extends Fragment {
+public class MasTardeFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
-    private FragmentGalleryBinding binding;
+    private MasTardeViewModel masTardeViewModel;
+    private FragmentMasTardeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        masTardeViewModel =
+                new ViewModelProvider(this).get(MasTardeViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentMasTardeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        masTardeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/proyectoandroid/ui/mastarde/MasTardeViewModel.java
+++ app/src/main/java/com/example/proyectoandroid/ui/mastarde/MasTardeViewModel.java
@@ -1,14 +1,14 @@
-package com.example.proyectoandroid.ui.gallery;
+package com.example.proyectoandroid.ui.mastarde;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class MasTardeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public MasTardeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
