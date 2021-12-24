d36aea1e9b9303cdc5adb9c1abccb488729ca5b5
Alvaro Vera Ortiz <alvaroveraortiz@uma.es>
2021-12-24 15:41:38 +0100

    Create FirestoreBD.java

---
+++ app/src/main/java/com/example/proyectoandroid/Resources/FirestoreBD.java
@@ -0,0 +1,10 @@
+package com.example.proyectoandroid.Resources;
+import com.google.firebas;
+
+public class FirestoreBD {
+    private final FirebaseFirestore db;
+
+    FirestoreBD(FirebaseFirestore db){
+        this.db = db;
+    }
+}
