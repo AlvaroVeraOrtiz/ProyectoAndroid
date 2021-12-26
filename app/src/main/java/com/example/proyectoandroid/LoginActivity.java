71d5583e2e04b3495ebce972052d677dae63aa8c
Alvaro Vera Ortiz <alvaroveraortiz@uma.es>
2021-12-26 21:37:52 +0100

    Hechos fragmentos de login
    Ahora mismo hay una actividad de login que no se usa. Dependiendo de si hace falta o no se añadirá

--- app/src/main/java/com/example/proyectoandroid/LoginActivity.java
+++ app/src/main/java/com/example/proyectoandroid/LoginActivity.java
@@ -1,18 +1,40 @@
 package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.proyectoandroid.ui.login.LoginFragment;
+import com.example.proyectoandroid.ui.login.RegisterFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mauth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
-    public void iniciarSesion(View view) {
+
+
+    @Override
+    public void onBackPressed(){
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.login_container);
+        if(fragment instanceof RegisterFragment){
+            getSupportFragmentManager().popBackStackImmediate();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/proyectoandroid/ui/login/RegisterFragment.java
@@ -0,0 +1,62 @@
+package com.example.proyectoandroid.ui.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.proyectoandroid.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RegisterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends Fragment {
+
+    private EditText correo, password, nombre;
+    private FirebaseAuth mauth = FirebaseAuth.getInstance();
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static RegisterFragment newInstance(FirebaseAuth auth) {
+        RegisterFragment fragment = new RegisterFragment();
+        fragment.setAuth(auth);
+        return fragment;
+    }
+    public void setAuth(FirebaseAuth auth) {
+        mauth = auth;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        correo = view.findViewById(R.id.correoDeRegistro);
+        nombre = view.findViewById(R.id.nombreDeRegistro);
+        password = view.findViewById(R.id.passwordDeRegistro);
+
+        return view;
+
+    }
+
+
+
+
+}
