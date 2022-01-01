b35b7f70cdc95c8a9f2f77eef7057061bd23d95d
GuillermoAguado <guilleam@uma.es>
2022-01-01 19:18:03 +0100

    🥳Add Watch Later Activity

---
+++ app/src/main/java/com/example/proyectoandroid/ui/watchlater/WatchLaterActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectoandroid.ui.watchlater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectoandroid.R;
+
+public class WatchLaterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_later);
+    }
+}
